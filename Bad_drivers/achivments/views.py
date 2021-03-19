061d0591c576c1b0fdf012ef80b869fd99d4e714
NickPYTH <nicksrnk@gmail.com>
2021-03-19 19:47:43 +0300

    added req for getting all achiv and user achiv

--- Bad_drivers/achivments/views.py
+++ Bad_drivers/achivments/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth.models import User
 from .serializers import AchivmentSerializer, UserAchivmentSerializer
 from rest_framework import generics
-from .models import Achivment
+from .models import Achivment, UserAchivment
 from rest_framework.response import Response
 
 class CreateAchivmentView(generics.CreateAPIView):
@@ -13,3 +13,23 @@ class CreateAchivmentView(generics.CreateAPIView):
 class CreateUserAchivmentView(generics.CreateAPIView):
     permission_classes = (IsAuthenticated,)
     serializer_class = UserAchivmentSerializer
+
+class GetAllAchivmentsList(generics.ListCreateAPIView):
+    queryset = Achivment.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = AchivmentSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = AchivmentSerializer(queryset.all(), many=True)
+        return Response(serializer.data)
+
+class GetUserAchivmentsList(generics.ListCreateAPIView):
+    queryset = UserAchivment.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserAchivmentSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = UserAchivmentSerializer(queryset.filter(user=request.user), many=True)
+        return Response(serializer.data)
