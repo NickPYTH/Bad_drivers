1122d7350eac2b23f43f041bfb7840cce5a67b16
de401t <sirenko2@edu.surgu.ru>
2021-06-03 23:00:03 +0500

    added db seeder

--- db_seeder.py
+++ db_seeder.py
@@ -2,6 +2,7 @@ import psycopg2
 from contextlib import closing
 
 class BadDriversSeed():
+    """Class for seeding testing values"""
     __cursor = None
     __connection = None
     def __init__(self):
@@ -10,27 +11,71 @@ class BadDriversSeed():
                     password="hello_django",
                     host="127.0.0.1",
                     port="5432",
-                    database="hello_django_dev")
+                    database="postgres")
         self.__cursor = self.__connection.cursor()
 
-    def reports_car_table_seed(self): #add args
-        try:
-            postgres_insert_query = """ INSERT INTO reports_car (car_number, car_region, car_country) VALUES (%s,%s,%s)"""
-            record_to_insert = (5, 1, 950)
-            self.__cursor.execute(postgres_insert_query, record_to_insert)
-            self.__connection.commit()
-            count = self.__cursor.rowcount
-            print(count, "Success")
+    def reports_car_table_seed(self, *args):
+        if len(args) == 3:
+            data_to_insert = []
+            for arg in args:
+                data_to_insert.append(arg)
+            try:
+                postgres_insert_query = """ INSERT INTO reports_car (car_number, car_region, car_country) VALUES (%s,%s,%s)"""
+                self.__cursor.execute(postgres_insert_query, data_to_insert)
+                self.__connection.commit()
+                count = self.__cursor.rowcount
+                print(count, "Success")
 
-        except (Exception, psycopg2.Error) as error:
-            print("Insert error: ", error)
+            except (Exception, psycopg2.Error) as error:
+                print("Insert error: ", error)
 
-        finally:
-            if self.__connection:
-                self.__cursor.close()
-                self.__connection.close()
-                print("Conection is closed")
+            finally:
+                if self.__connection:
+                    self.__cursor.close()
+                    self.__connection.close()
+                    print("Conection is closed")
 
-a = BadDriversSeed()
+    def auth_users_table_seed(self, *args):
+        if len(args) == 10:
+            data_to_insert = []
+            for arg in args:
+                data_to_insert.append(arg)
+            try:
+                postgres_insert_query = """ INSERT INTO public.auth_user (id, password, last_login, is_superuser, username, first_name, last_name, email, is_staff,
+                              is_active, date_joined)
+                                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
+                self.__cursor.execute(postgres_insert_query, data_to_insert)
+                self.__connection.commit()
+                count = self.__cursor.rowcount
+                print(count, "Success")
 
-a.reports_car_table_seed()
+            except (Exception, psycopg2.Error) as error:
+                print("Insert error: ", error)
+
+            finally:
+                if self.__connection:
+                    self.__cursor.close()
+                    self.__connection.close()
+                    print("Conection is closed")
+    
+    def reports_car_table_seed(self, *args):
+        if len(args) == 3:
+            data_to_insert = []
+            for arg in args:
+                data_to_insert.append(arg)
+            try:
+                postgres_insert_query = """ INSERT INTO reports_car (car_number, car_region, car_country) VALUES (%s,%s,%s)"""
+                self.__cursor.execute(postgres_insert_query, data_to_insert)
+                self.__connection.commit()
+                count = self.__cursor.rowcount
+                print(count, "Success")
+
+            except (Exception, psycopg2.Error) as error:
+                print("Insert error: ", error)
+
+            finally:
+                if self.__connection:
+                    self.__cursor.close()
+                    self.__connection.close()
+                    print("Conection is closed")
+        
